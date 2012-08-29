adbaf3cbd61f81a9c5eb6ae9990fceadbf926314
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-08-30 01:53:05 +0530

    bump version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clip-test "0.1.0-SNAPSHOT"
+(defproject clip-test "0.1.0"
   :description "Subset of `clojure.test` for Clojure and ClojureScript"
   :url "https://github.com/kumarshantanu/clip-test"
   :license {:name "Eclipse Public License"
